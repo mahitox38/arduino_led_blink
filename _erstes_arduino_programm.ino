94b4c433b2b219d7a75705fde170a99869c218b8
Muhammed Eyyub Serim <muhamedserim273@gmail.com>
2025-02-05 01:53:26 +0300

    Mein erstes Arduino-Programm zum Blinken einer LED

---
+++ _erstes_arduino_programm.ino
@@ -0,0 +1,41 @@
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); 
+  delay(500);    
+  digitalWrite(ledPin, LOW);
+  delay(1500);
+}
+
+
+/*
+int ledPin = 3;
+int pmValue = 0;
+
+void setup(){
+
+}
+
+
+void loop() {
+  analogWrite(ledPin, pmValue++);
+  if(pmValue > 254) pmValue = 0;
+  delay(10);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
